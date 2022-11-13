3e67f76617a7b17ef7a5b83e2ed4cd6c74bf2d13
Данил Штунь <danilshtun@yandex.ru>
2022-11-13 18:44:53 +0500

    flake8

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -2,8 +2,8 @@ from django.contrib.auth.models import AbstractUser
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db.models import (CASCADE, CharField, DateTimeField, EmailField,
                               ForeignKey, ImageField, ManyToManyField, Model,
-                              PositiveSmallIntegerField, SlugField,
-                              TextField, UniqueConstraint)
+                              PositiveSmallIntegerField, SlugField, TextField,
+                              UniqueConstraint)
 from foodgram.settings import (MAX_LEN_RECIPES_CHARFIELD,
                                MAX_LEN_USERS_CHARFIELD)
 
