3df80e4dc6d8707a8330a6be774b2123fcaf9efc
Данил Штунь <danilshtun@yandex.ru>
2022-11-13 14:47:29 +0500

    fix urls rm djoser url from

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -12,6 +12,5 @@ router.register(r'users', CustomUserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
 ]
