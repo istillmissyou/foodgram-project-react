06100c3d17a06d0b00b4dca1c4c0bef40c1b923b
Данил Штунь <danilshtun@yandex.ru>
2022-11-21 07:59:00 +0500

    fix urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,19 +3,20 @@ from rest_framework.routers import DefaultRouter
 
 from .views import (DownloadShoppingCart, FavoriteApiView, FollowApiView,
                     FollowListApiView, IngredientViewSet, RecipeViewSet,
-                    ShoppingView, TagViewSet, UserViewSet)
+                    ShoppingView, TagViewSet)
 
 router = DefaultRouter()
-router.register(r'tags', TagViewSet, 'tags')
+router.register('tags', TagViewSet, 'tags')
 router.register(r'ingredients', IngredientViewSet, 'ingredients')
-router.register(r'recipes', RecipeViewSet, 'recipes')
-router.register('users', UserViewSet, 'users')
+router.register('recipes', RecipeViewSet, 'recipes')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('users/subscriptions/', FollowListApiView.as_view()),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    path('users/<int:following_id>/subscribe/', FollowApiView.as_view()),
     path('recipes/download_shopping_cart/', DownloadShoppingCart.as_view()),
     path('recipes/<int:favorite_id>/favorite/', FavoriteApiView.as_view()),
     path('recipes/<int:recipe_id>/shopping_cart/', ShoppingView.as_view()),
-    path('users/subscriptions/', FollowListApiView.as_view()),
-    path('users/<int:following_id>/subscribe/', FollowApiView.as_view()),
 ]
